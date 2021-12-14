6af6f7865b404787451c86d341195f3c910dbdef
t-plasmid <82651617+t-plasmid@users.noreply.github.com>
2021-12-14 22:11:51 +0000

    Update settings.py

--- Army_Calc_Django_Web_Project/Army_Calc_Django_Web_Project/settings.py
+++ Army_Calc_Django_Web_Project/Army_Calc_Django_Web_Project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+importdjango_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,3 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
